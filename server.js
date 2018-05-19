a5441ba6409fe1953abd4a6d228d4297505864fd
abdelrhman <abdelrhmantalat.93@gmail.com>
2018-05-19 18:21:27 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = new express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  console.log(now);
+  next();
+  // res.render('about.hbs', {
+  //   title: 'this is page title',
+  //   year: new Date().getFullYear()
+  // });
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    title: 'this is home title',
+    year: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'this is page title',
+    year: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    message: 'error '
+  });
+});
+
+app.listen(3000);
